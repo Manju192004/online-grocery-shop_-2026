b1031e0d71908a558ab9f4c92a178ed1ce762ccf
Manju192004 <manjuseenivasan2004@gmail.com>
2026-07-02 15:55:00 +0530

    Update add_original_price.py

--- add_original_price.py
+++ add_original_price.py
@@ -4,7 +4,7 @@ try:
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="", # using empty password as in app.py
+        password="", 
         database="homedb"
     )
     cursor = conn.cursor()
